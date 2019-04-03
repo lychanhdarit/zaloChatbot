96f201e47bc96ffba8c371369a7d657c1bbd8010
Da Ric <lychanhdarit@gmail.com>
2019-04-03 13:51:31 +0700

    update

--- index.js
+++ index.js
@@ -15,21 +15,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/webhook/',function(req,res){ 
-	console.log(JSON.stringify(req.query));
-	console.log(JSON.stringify(req.body));
-	console.log("Query Post: "+req.query);
-});
-
-app.get('/webhook/',(req,res)=>{
-	console.log(JSON.stringify(req.query));
-    console.log("Query: "+req.query);
-	replyMessageServer('7469704234848807541',"Query: "+req.query.event);
+	//console.log(JSON.stringify(req.query));
+	//console.log("Query post: "+req.query);
+	
+	//replyMessageServer('7469704234848807541',"Query: "+req.query.event);
 	
 	//var userId = '84964900534';
 		//ZOAClient.api('getprofile', { uid: userId }, function(response) {
 		//console.log(response);
 	//});
-	
+	/*
     switch(req.query.event){
         case 'sendmsg':
         let message = req.query.message;
@@ -42,12 +37,44 @@ app.get('/webhook/',(req,res)=>{
 			replyMessage(req.query.fromuid);
 			break;
 
+	}*/
+	
+    res.sendStatus(200);
+});
+
+app.get('/webhook/',(req,res)=>{
+    console.log("Query: "+req.query.event);
+	//replyMessageServer('7469704234848807541',"Query: "+req.query.event);
+	//var userId = '84964900534';
+		//ZOAClient.api('getprofile', { uid: userId }, function(response) {
+		//console.log(response);
+	//});
+    switch(req.query.event){
+        case 'sendmsg':
+			let message = req.query.message;
+			switch(message){
+				case 'hello':
+				replyMessage(req.query.fromuid);
+				break;
+			}
+			break;
+		case 'follow':
+			replyMessageFollower(req.query.fromuid);
+			break;
+
+
     }
     res.sendStatus(200);
 });
 function replyMessage(userId){ 
-    ZOAClient.api('sendmessage/text', 'POST', {uid: userId, message: 'APICODE'}, function(response) {
-	console.log("Query 2: "+response);
+    ZOAClient.api('sendmessage/text', 'POST', {uid: userId, message: 'VINASAVE xin chào quý khách.'}, function(response) {
+	console.log("Send message: "+response);
+	console.log(JSON.stringify(response));
+    })
+}
+function replyMessageFollower(userId){ 
+    ZOAClient.api('sendmessage/text', 'POST', {uid: userId, message: 'Xin chúc mừng quý khách đã nhận được mã giảm giá 10% dành riêng cho khách hàng quan tâm VINASAVE trên Zalo trong tháng 4/2019.\n\nMã giảm giá của bạn: ZaloVNS (Áp dụng đến hết 30/4/2019) \n\nXem sản phẩm mới nhất trên website tại đây: http://bit.ly/2VaZvEQ'}, function(response) {
+	console.log("Send message: "+response);
 	console.log(JSON.stringify(response));
     })
 }
