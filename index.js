5671eb3c34b00d67817fe865d4f4b1649916be9f
Đa Ric <lychanhdarit@yahoo.com.vn>
2019-04-09 13:30:56 +0700

    update message

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function replyMessage(userId){
     })
 }
 function replyMessageFollower(userId){ 
-    ZOAClient.api('sendmessage/text', 'POST', {uid: userId, message: 'Xin chúc mừng quý khách đã nhận được mã giảm giá 10% dành riêng cho khách hàng quan tâm VINASAVE trên Zalo trong tháng 4/2019.\n\nMã giảm giá của bạn: ZaloVNS (Áp dụng đến hết 30/4/2019) \n\nXem sản phẩm mới nhất trên website tại đây: http://bit.ly/2VaZvEQ'}, function(response) {
+    ZOAClient.api('sendmessage/text', 'POST', {uid: userId, message: 'Xin chúc mừng quý khách đã nhận được mã giảm giá 10% dành riêng cho khách hàng quan tâm VINASAVE trên Zalo trong tháng 4/2019.\n\nMã giảm giá của bạn: ZaloVNS (Áp dụng đến hết 30/4/2019) \n\nNhập mã khi mua hàng online tại website: http://bit.ly/2VaZvEQ'}, function(response) {
 		console.log("Send message: "+response); 
 	}); 
 	saveDataFollow(userId);
